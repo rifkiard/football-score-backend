33c9c0009222fe8a24bcad9b4aa086c4e4db1788
rifkiard <rifkiardiansyah670@gmail.com>
2021-06-16 21:16:08 +0700

    connenecting to MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+// Connect to DB
+mongoose.connect("mongodb+srv://rifki:Rifkiamanda1@cluster0.mvvnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", () => {
+    console.log("connected to db")
+})
 
 // Import routes
 const authRoute = require("./routes/auth")
 
 // Route Middlewares
 
-app.use('/api/user', authRoute)
+app.use("/api/user", authRoute)
 
 app.listen(3000, () => console.log("Server up and running"))
