33d90dccd02ae17ec438538f82e2a2877ede3050
rifkiard <rifkiardiansyah670@gmail.com>
2021-06-16 21:02:07 +0700

    Auth route -added

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express')
-const app = exporess()
+const app = express()
+
+// Import routes
+const authRoute = require("./routes/auth")
+
+// Route Middlewares
+
+app.use('/api/user', authRoute)
 
 app.listen(3000, () => console.log("Server up and running"))
